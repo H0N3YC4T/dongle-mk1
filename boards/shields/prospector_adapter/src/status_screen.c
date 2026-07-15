2e319f5290ba9ce99455ed5b447b66f0864022c6
Honeycat <andrew.tsakas@gmail.com>
2026-07-16 01:14:51 +1000

    Update status_screen.c

--- boards/shields/prospector_adapter/src/status_screen.c
+++ boards/shields/prospector_adapter/src/status_screen.c
@@ -20,63 +20,80 @@ static struct zmk_widget_layer_display layer_display_widget;
 static struct zmk_widget_battery_circles battery_circles_widget;
 static struct zmk_widget_output output_widget;
 
-void status_screen_reflow(void) {
-    if (ui_rot & 1) {
-        zmk_widget_modifier_indicator_set_width(&modifier_indicator_widget, 200);
-        lv_obj_set_pos(zmk_widget_modifier_indicator_obj(&modifier_indicator_widget), 20, 8);
-        zmk_widget_wpm_meter_set_width(&wpm_meter_widget, 220);
-        lv_obj_set_pos(zmk_widget_wpm_meter_obj(&wpm_meter_widget), 10, 40);
-        zmk_widget_layer_display_set_width(&layer_display_widget, 220);
-        lv_obj_set_pos(zmk_widget_layer_display_obj(&layer_display_widget), 10, 140);
-        zmk_widget_output_set_stacked(true);
-        zmk_widget_battery_circles_place(220, 62, 0, 2, 162, 2);
-        lv_obj_set_pos(zmk_widget_battery_circles_obj(&battery_circles_widget), 10, 154);
-        lv_obj_set_pos(zmk_widget_output_obj(&output_widget), 10, 154);
-    } else {
-        zmk_widget_modifier_indicator_set_width(&modifier_indicator_widget, 230);
-        lv_obj_set_pos(zmk_widget_modifier_indicator_obj(&modifier_indicator_widget), 25, 8);
-        zmk_widget_wpm_meter_set_width(&wpm_meter_widget, 260);
-        lv_obj_set_pos(zmk_widget_wpm_meter_obj(&wpm_meter_widget), 10, 42);
-        zmk_widget_layer_display_set_width(&layer_display_widget, 260);
-        lv_obj_set_pos(zmk_widget_layer_display_obj(&layer_display_widget), 10, 142);
-        zmk_widget_output_set_stacked(false);
-        zmk_widget_battery_circles_place(256, 62, 0, 2, 198, 2);
-        lv_obj_set_pos(zmk_widget_battery_circles_obj(&battery_circles_widget), 12, 170);
-        lv_obj_set_pos(zmk_widget_output_obj(&output_widget), 82, 170);
-    }
+void status_screen_reflow(void)
+{
+  if (ui_rot & 1)
+  {
+    zmk_widget_modifier_indicator_set_width(&modifier_indicator_widget, 200);
+    lv_obj_set_pos(zmk_widget_modifier_indicator_obj(&modifier_indicator_widget), 20, 8);
+
+    zmk_widget_wpm_meter_set_width(&wpm_meter_widget, 220);
+    lv_obj_set_pos(zmk_widget_wpm_meter_obj(&wpm_meter_widget), 10, 40);
+
+    zmk_widget_layer_display_set_width(&layer_display_widget, 220);
+    lv_obj_set_pos(zmk_widget_layer_display_obj(&layer_display_widget), 10, 140);
+
+    zmk_widget_output_set_stacked(true);
+
+    zmk_widget_battery_circles_place(220, 62, 0, 2, 162, 2);
+    lv_obj_set_pos(zmk_widget_battery_circles_obj(&battery_circles_widget), 10, 154);
+
+    lv_obj_set_pos(zmk_widget_output_obj(&output_widget), 10, 154);
+  }
+  else
+  {
+    zmk_widget_modifier_indicator_set_width(&modifier_indicator_widget, 230);
+    lv_obj_set_pos(zmk_widget_modifier_indicator_obj(&modifier_indicator_widget), 25, 8);
+
+    zmk_widget_wpm_meter_set_width(&wpm_meter_widget, 260);
+    lv_obj_set_pos(zmk_widget_wpm_meter_obj(&wpm_meter_widget), 10, 42);
+
+    zmk_widget_layer_display_set_width(&layer_display_widget, 260);
+    lv_obj_set_pos(zmk_widget_layer_display_obj(&layer_display_widget), 10, 142);
+
+    zmk_widget_output_set_stacked(false);
+
+    zmk_widget_battery_circles_place(256, 62, 0, 2, 198, 2);
+    lv_obj_set_pos(zmk_widget_battery_circles_obj(&battery_circles_widget), 12, 170);
+
+    lv_obj_set_pos(zmk_widget_output_obj(&output_widget), 82, 170);
+  }
 }
 
 static lv_obj_t *status_screen_obj;
 
 /* live retheme: restyle the status widgets in place (touch views rebuild
  * themselves via build_view) */
-void theme_changed(void) {
-    if (status_screen_obj == NULL) {
-        return;
-    }
-    lv_obj_set_style_bg_color(status_screen_obj, lv_color_hex(theme_color(THEME_BACKGROUND)),
-                              LV_PART_MAIN);
-    zmk_widget_wpm_meter_retheme();
-    zmk_widget_modifier_indicator_retheme();
-    zmk_widget_battery_circles_retheme();
-    zmk_widget_output_retheme();
+void theme_changed(void)
+{
+  if (status_screen_obj == NULL)
+  {
+    return;
+  }
+  lv_obj_set_style_bg_color(status_screen_obj, lv_color_hex(theme_color(THEME_BACKGROUND)),
+                            LV_PART_MAIN);
+  zmk_widget_wpm_meter_retheme();
+  zmk_widget_modifier_indicator_retheme();
+  zmk_widget_battery_circles_retheme();
+  zmk_widget_output_retheme();
 }
 
-lv_obj_t *zmk_display_status_screen() {
-    lv_obj_t *screen = lv_obj_create(NULL);
-    status_screen_obj = screen;
-    lv_obj_set_style_bg_color(screen, lv_color_hex(theme_color(THEME_BACKGROUND)), LV_PART_MAIN);
-    lv_obj_set_style_bg_opa(screen, 255, LV_PART_MAIN);
+lv_obj_t *zmk_display_status_screen()
+{
+  lv_obj_t *screen = lv_obj_create(NULL);
+  status_screen_obj = screen;
+  lv_obj_set_style_bg_color(screen, lv_color_hex(theme_color(THEME_BACKGROUND)), LV_PART_MAIN);
+  lv_obj_set_style_bg_opa(screen, 255, LV_PART_MAIN);
 
-    zmk_widget_modifier_indicator_init(&modifier_indicator_widget, screen);
-    zmk_widget_wpm_meter_init(&wpm_meter_widget, screen);
-    zmk_widget_layer_display_init(&layer_display_widget, screen);
-    zmk_widget_battery_circles_init(&battery_circles_widget, screen);
-    zmk_widget_output_init(&output_widget, screen);
-    status_screen_reflow();
+  zmk_widget_modifier_indicator_init(&modifier_indicator_widget, screen);
+  zmk_widget_wpm_meter_init(&wpm_meter_widget, screen);
+  zmk_widget_layer_display_init(&layer_display_widget, screen);
+  zmk_widget_battery_circles_init(&battery_circles_widget, screen);
+  zmk_widget_output_init(&output_widget, screen);
+  status_screen_reflow();
 
-    touch_ui_attach(screen);
-    touch_prefs_apply(); /* restore saved brightness/sensitivity/rotation */
+  touch_ui_attach(screen);
+  touch_prefs_apply(); /* restore saved brightness/sensitivity/rotation */
 
-    return screen;
+  return screen;
 }
